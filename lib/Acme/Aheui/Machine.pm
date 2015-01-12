810b4c64fcdbbd540da3a9c88272e0220832e1cc
rakjin <grampush@gmail.com>
2015-01-12 16:37:17 +0900

    add BUILD and codespace to Machine

--- lib/Acme/Aheui/Machine.pm
+++ lib/Acme/Aheui/Machine.pm
@@ -2,12 +2,31 @@ package Acme::Aheui::Machine;
 use Moose;
 use namespace::autoclean;
 
-has 'source' => (
+has '_source' => (
     is => 'ro',
     isa => 'Str',
+    init_arg => 'source',
     required => 1,
 );
 
+has '_codespace' => (
+    is => 'rw',
+    isa => 'ArrayRef[ArrayRef[HashRef]]',
+    init_arg => undef,
+);
+
+sub BUILD {
+    my ($self, $args) = @_;
+
+    my $codespace = $self->_build_codespace($self->_source);
+    $self->_codespace($codespace);
+}
+
+sub _build_codespace {
+    my ($self, $source) = @_;
+
+    return [[{}]];
+}
 
 __PACKAGE__->meta->make_immutable;
 1;
