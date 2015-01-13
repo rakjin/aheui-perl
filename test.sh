83356ec2bb6c85dad0242554638d555076273351
rakjin <grampush@gmail.com>
2015-01-13 17:36:40 +0900

    copy test.sh from naheui

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -e snippets ]; then
+    cd snippets
+    git pull
+else
+    git clone https://github.com/aheui/snippets
+    cd snippets
+fi
+
+chmod 755 ../lib/aheui.pl
+AHEUI="perl -I ../lib ../lib/aheui.pl" bash test.sh standard
+cd ..
+
