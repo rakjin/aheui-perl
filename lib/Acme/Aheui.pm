5e310749019d999d4267b22feb812c45bc7a564c
grampush <grampush@gmail.com>
2015-01-24 02:11:08 +0900

    (trivial) metadata

--- lib/Acme/Aheui.pm
+++ lib/Acme/Aheui.pm
@@ -429,9 +429,7 @@ To install this module, run the following commands:
 
 Rakjin Hwang, C<< <rakjin@cpan.org> >>
 
-=head1 COPYRIGHT AND LICENSE
-
-Copyright 2015 Rakjin Hwang.
+=head1 LICENSE
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
